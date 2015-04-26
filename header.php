3c869af0d7267aaef92d9d4e30da105ac68a90ce
design311 <hello@design311.com>
2015-04-26 02:42:31 +0100

    remove facebook pixel from header

--- header.php
+++ header.php
@@ -54,23 +54,6 @@
 
 		</script>
 
-		<!-- Facebook Conversion Code for Poco Loco Algemeen -->
-		<script>(function() {
-		var _fbq = window._fbq || (window._fbq = []);
-		if (!_fbq.loaded) {
-		var fbds = document.createElement('script');
-		fbds.async = true;
-		fbds.src = '//connect.facebook.net/en_US/fbds.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(fbds, s);
-		_fbq.loaded = true;
-		}
-		})();
-		window._fbq = window._fbq || [];
-		window._fbq.push(['track', '6026708722107', {'value':'0.01','currency':'EUR'}]);
-		</script>
-		<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6026708722107&amp;cd[value]=0.01&amp;cd[currency]=EUR&amp;noscript=1" /></noscript>
-
 		<?php wp_head(); ?>
 	</head>
 
